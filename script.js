d565065a786ff5f8b03c847ba6791e89d78cdb7e
Marcos Paulo Badaró <marcospaulobadaro@gmail.com>
2022-08-15 17:16:26 -0300

    primeira parte do menu completa

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 const img = document.querySelector('.icon');
+const popup = document.querySelector('.principal-menu');
 let num = 0;
 
 // console.log(img);
 // img.src = "./images/icon-hamburger.svg";
 // menu.appendChild(img);
 
+
+
 img.addEventListener('click', () => {
      num += 1
      if(num % 2 == 1){
         img.style.backgroundImage = 'url(./images/icon-close.svg)'
+        popup.classList.add('active')
+
      } else {
         img.style.backgroundImage = 'url(./images/icon-hamburger.svg)'
+        popup.classList.remove('active')
      }
 
 })
+
+console.log(popup)
 
